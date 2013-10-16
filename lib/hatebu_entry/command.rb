b921cd77215287edb64a4d63c497162d80075c1e
kyoendo <postagie@gmail.com>
2013-10-16 14:08:50 +0900

    Add merge command

--- lib/hatebu_entry/command.rb
+++ lib/hatebu_entry/command.rb
@@ -1,16 +1,31 @@
 require 'thor'
 
 class HatebuEntry::Command < Thor
-  desc "get [URL]", "Get Hatena bookmark entries for URL"
-  option :sort, aliases:"-s", default:"count"
+  desc "get URL", "Get Hatena bookmark entries for URL"
   option :pages, aliases:"-p", default:0
+  option :sort, aliases:"-s", default:"count"
   def get(url)
-    hbent = HatebuEntry.new(url, options[:sort])
-    pretty_print hbent.entries(options[:pages].to_i)
+    entries = HatebuEntry.new(url, options[:sort])
+                         .entries(options[:pages].to_i)
+    pretty_print begin
+      options[:sort]=='count' ? entries.sort_by { |ent| -ent.count } : entries
+    end
   rescue
     abort "something go wrong."
   end
 
+  desc "merge *URLs", "Merge counts for same entries on several URLs"
+  option :pages, aliases:"-p", default:1
+  option :sort, aliases:"-s", default:"count"
+  def merge(*urls)
+    abort "At least 2 urls needed." if urls.size < 2
+    entries = urls.map do |url|
+      HatebuEntry.new(url, options[:sort]).entries options[:pages].to_i
+    end
+    merged = entries.inject {|mem, ent| HatebuEntry::Entry.merge(mem, ent) }
+    pretty_print merged.sort_by { |ent| -ent.count }
+  end
+  
   desc "version", "Show HatebuEntry version"
   def version
     puts "HatebuEntry #{HatebuEntry::VERSION} (c) 2013 kyoendo"
